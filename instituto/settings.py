a4d1188006bdfb03d34f038fed28c2ef0326715e
MrNoctur <163854376+MrNoctur@users.noreply.github.com>
2024-06-24 19:23:10 -0400

    Revert "cambio de nombre"
    This reverts commit adcf0fc167e69909956fc278cc5b4f3182881a81.

--- instituto/settings.py
+++ instituto/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'tienda',
+    'alumnos',
 ]
 
 MIDDLEWARE = [
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'instituto.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'tienda' , 'templates')],
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -117,9 +117,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
-staticfiles_dirs = [
-    os.path.join(BASE_DIR, 'tienda', 'static'),
-]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
